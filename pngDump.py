9681061f2b7523b4640e397a9f315b26f1663824
Ben Wells <benfred@mjaw.com>
2024-05-02 16:22:38 +1000

    Remembering python; printing palette

--- pngDump.py
+++ pngDump.py
@@ -1,9 +1,19 @@
-from pngparser import PngParser, chunktypes
+from pngparser import PngParser, TYPE_PLTE
 import sys
 
 if __name__ == "__main__":
-  print(f"Dumping info for {sys.argv[1]}")
+  print(f'Dumping info for {sys.argv[1]}')
   png = PngParser(sys.argv[1])
   header = png.get_header()
   print(header)
   print(header.use_palette())
+  palette_chunks = png.get_by_type(TYPE_PLTE)
+  for chunk in palette_chunks:
+    print('Have a chunk')
+    print(f'size {len(chunk.data)}')
+    for i in range(0, len(chunk.data)//3):
+      base = i * 3
+      r = chunk.data[base]
+      g = chunk.data[base+1]
+      b = chunk.data[base+2]
+      print(f'color {i} is r:{r:2x} g:{g:2x} b:{b:2x}')
